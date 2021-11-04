be25373454c813f729272755adff955fe023c0eb
nate smith <vilmibm@github.com>
2021-11-04 15:35:44 -0700

    fix release when not signing

--- build_and_release.sh
+++ build_and_release.sh
@@ -3,6 +3,7 @@ set -e
 
 tag=$(git describe --tags --abbrev=0)
 platforms=$(echo $GH_EXT_PLATFORMS | tr "," "\n")
+include="dist/*"
 
 if [ -n "${GH_EXT_BUILD_SCRIPT}" ]; then
   echo "invoking build script override ${GH_EXT_BUILD_SCRIPT}"
@@ -17,11 +18,13 @@ fi
 
 ls -A dist >/dev/null || (echo "no files found in dist/" && exit 1)
 
+
 if [ -n "${GPG_FINGERPRINT}" ]; then
   for f in $(ls dist); do
     shasum -a 256 dist/$f >> checksums.txt
   done
   gpg --output checksums.txt.sig --detach-sign checksums.txt
+  include="dist/* checksums*"
 fi
 
 prerelease=""
@@ -33,4 +36,5 @@ fi
 gh api repos/$GITHUB_REPOSITORY/releases/generate-notes \
   -f tag_name="${tag}" -q .body > CHANGELOG.md
 
-gh release create $tag $prerelease --notes-file CHANGELOG.md dist/* checksums*
+
+gh release create $tag $prerelease --notes-file CHANGELOG.md $include
