fafc634d6cf4deee5fb6b1937ca2a6b74cc9bbdd
Bertie690 <taylormw163@gmail.com>
2025-10-24 18:05:34 -0400

    Update logging messages + add coloration
    yes i have bash ansi color coding functions up my back pocket don't ask

---
+++ utils.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+# Assorted color utilities for build_and_release.sh.
+
+# Make text orange.
+orange () {
+  printf "\e[33m%s\e[0m" "$1"
+}
+
+# Make text red.
+red () {
+  printf "\e[31m%s\e[0m" "$1"
+}
+
+# Make text green.
+green () {
+  printf "\e[32m%s\e[0m" "$1"
+}
+
+# Make text blue.
+blue () {
+  printf "\e[34m%s\e[0m" "$1"
+}
+
+# Annotate text with a blue [...].
+info () {
+  printf "[$(blue ...)] %s\n" "$1"
+}
+
+# Annotate text with a green [✔].
+success () {
+  printf "[$(green '✔ ')] %s\n" "$1"
+}
+
+# Annotate text with an orange [!!!] and send to stderr.
+warn () {
+  printf "[$(orange !!!)] %s\n" "$1" >&2
+}
+
+# Annotate text with a red [✗] and send to stderr.
+error () {
+  printf "[ $(red '✗') ] %s\n" "$1" >&2
+}
+
+# Annotate text with a red [✗], and then return the specified non-zero to kill the program.
+# Defaults to 1 if not provided.
+fail () {
+  error "$1"
+  return "${2:-1}"
+}
