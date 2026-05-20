93e7745108303bde30e0398d94f36bff668e2fd7
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2026-05-20 08:39:39 -0400

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -31,18 +31,19 @@ success () {
   printf "[$(green '✔ ')] %s\n" "$1"
 }
 
-# Annotate text with an orange [!!!] and send to stderr.
+# Annotate text with an orange [!!!] and send it to stderr.
 warn () {
   printf "[$(orange !!!)] %s\n" "$1" >&2
 }
 
-# Annotate text with a red [✗] and send to stderr.
+# Annotate text with a red [✗] and send it to stderr.
 error () {
   printf "[ $(red '✗') ] %s\n" "$1" >&2
 }
 
-# Annotate text with a red [✗], and then return the specified non-zero to kill the program.
-# Defaults to 1 if not provided.
+# Annotate text with a red [✗] and send it to stderr,
+# then return the specified non-zero error code to kill the program.
+# Defaults to error code 1 if not provided.
 fail () {
   error "$1"
   return "${2:-1}"
